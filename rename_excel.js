726bf7f032398a1c26c403353d22383cf20a9e2a
Muhammad Faisal Peerzada <mohammadfaisalpirzada@gmail.com>
2025-06-26 16:52:23 +0500

    Version 5

--- rename_excel.js
+++
@@ -1,9 +0,0 @@
-// This script renames teachers.xlsx to staffdata.xlsx in the correct public/data folder
-const fs = require('fs');
-const path = require('path');
-
-const oldPath = path.join(__dirname, './public/data/teachers.xlsx');
-const newPath = path.join(__dirname, './public/data/staffdata.xlsx');
-
-fs.renameSync(oldPath, newPath);
-console.log('Renamed teachers.xlsx to staffdata.xlsx');
